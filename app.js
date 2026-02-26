d65f27a01e504b40c25955e117d1cce503aeb721
rajeshark <101121907+rajeshark@users.noreply.github.com>
2026-02-26 17:29:54 +0530

    Change greeting message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Microservice');
+  res.send('Hello from rajesh');
 });
 
 const PORT = 3000;
