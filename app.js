adf165e51dd1fae72b3ab3132258efe372421451
rajeshark <101121907+rajeshark@users.noreply.github.com>
2026-02-26 11:15:15 +0530

    Change response message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Microservice');
+  res.send('Hello from Microservice1');
 });
 
 const PORT = 3000;
